8e9234f687733003ca399291cf06a9b0bb731b3d
立党 Lidang <wxr001@gmail.com>
2023-03-31 13:53:10 -0500

    Create batch_create_transfer_tokens_test.ts

---
+++ test/operationUnitTest/batch_create_transfer_tokens_test.ts
@@ -0,0 +1,80 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+
+// test for batch mint token instruction on DARC
+
+describe("batch_mint_token_test", function () {
+
+  
+  it ("should mint tokens", async function () {
+
+    const DARC = await ethers.getContractFactory("DARC");
+    const darc = await DARC.deploy();
+    console.log("DARC address: ", darc.address);
+    await darc.deployed();
+    await darc.initialize();
+
+
+    const programOperatorAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+
+
+    // create a token class first
+    await darc.entrance({
+      programOperatorAddress: programOperatorAddress,
+      operations: [{
+        operatorAddress: programOperatorAddress,
+        opcode: 2, // create token class
+        param: {
+          UINT256_ARRAY: [],
+          ADDRESS_ARRAY: [],
+          STRING_ARRAY: ["Class1", "Class2"],
+          BOOL_ARRAY: [],
+          VOTING_RULE_ARRAY: [],
+          PARAMETER_ARRAY: [],
+          PLUGIN_ARRAY: [],
+          UINT256_2DARRAY: [
+            [BigNumber.from(0), BigNumber.from(1)],
+            [BigNumber.from(10), BigNumber.from(1)],
+            [BigNumber.from(10), BigNumber.from(1)],
+          ],
+          ADDRESS_2DARRAY: []
+        }
+      }], 
+    });
+
+    // mint tokens to myself
+    await darc.entrance({
+      programOperatorAddress: programOperatorAddress,
+      operations: [{
+        operatorAddress: programOperatorAddress,
+        opcode: 1, // mint token
+        param: {
+          UINT256_ARRAY: [],
+          ADDRESS_ARRAY: [],
+          STRING_ARRAY: [],
+          BOOL_ARRAY: [],
+          VOTING_RULE_ARRAY: [],
+          PARAMETER_ARRAY: [],
+          PLUGIN_ARRAY: [],
+          UINT256_2DARRAY: [
+            [BigNumber.from(0), BigNumber.from(1)],  // token class = 0
+            [BigNumber.from(100), BigNumber.from(200)], // amount = 100
+          ],
+          ADDRESS_2DARRAY: [
+            [programOperatorAddress,programOperatorAddress], // to = programOperatorAddress
+          ]
+        }
+      }], 
+    });
+
+    // get the address balance of token 0 and 1
+    const balance0 = await darc.getTokenOwnerBalance(0, programOperatorAddress);
+    const balance1 = await darc.getTokenOwnerBalance(1, programOperatorAddress);
+
+    expect(balance0.toBigInt().toString()).to.equal("100");
+    expect(balance1.toBigInt().toString()).to.equal("200");
+  });
+});
