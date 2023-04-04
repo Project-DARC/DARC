60b528457ed77a318f6e0f8e171e44ab88295307
立党 Lidang <wxr001@gmail.com>
2023-04-04 18:00:17 -0500

    Update batch_burn_tokens_from_test.ts

--- test/operationUnitTest/batch_burn_tokens_from_test.ts
+++ test/operationUnitTest/batch_burn_tokens_from_test.ts
@@ -6,10 +6,10 @@ import { BigNumber } from "ethers";
 
 // test for batch mint token instruction on DARC
 
-describe("batch_burn_tokens_test", function () {
+describe("batch_burn_tokens_from_to_test", function () {
 
   
-  it ("should burn tokens", async function () {
+  it ("should burn tokens (from-to)", async function () {
 
     const DARC = await ethers.getContractFactory("DARC");
     const darc = await DARC.deploy();
@@ -22,7 +22,9 @@ describe("batch_burn_tokens_test", function () {
 
     const target1 = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
 
+    const target2 = '0x90F79bf6EB2c4f870365E785982E1f101E93b906';
 
+    const target3 = '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65';
 
     // create a token class first
     await darc.entrance({
@@ -68,13 +70,13 @@ describe("batch_burn_tokens_test", function () {
             [BigNumber.from(100), BigNumber.from(200)], // amount = 100
           ],
           ADDRESS_2DARRAY: [
-            [programOperatorAddress,programOperatorAddress], // to = target 1
+            [target2,target3], // to = target 2, target 3
           ]
         }
       },
       {
         operatorAddress: programOperatorAddress,
-        opcode: 5, // burn tokens
+        opcode: 6, // burn tokens from target 2 and target 3
         param:{
           UINT256_ARRAY: [],
           ADDRESS_ARRAY: [],
@@ -87,7 +89,9 @@ describe("batch_burn_tokens_test", function () {
             [BigNumber.from(0),BigNumber.from(1)],  // token class = 0, 1
             [BigNumber.from(10), BigNumber.from(40)], // amount = 10, 40
           ],
-          ADDRESS_2DARRAY: []
+          ADDRESS_2DARRAY: [
+            [target2,target3], // from = target 2, target 3
+          ]
         }
       }], 
     });
@@ -95,7 +99,7 @@ describe("batch_burn_tokens_test", function () {
     // check balance of programOperatorAddress:
     // class 0 = 100 -10 = 90
     // class 1 = 200 - 40 = 160
-    expect ((await darc.getTokenOwnerBalance(0, programOperatorAddress)).toBigInt().toString()).to.equal("90");
-    expect ((await darc.getTokenOwnerBalance(1, programOperatorAddress)).toBigInt().toString()).to.equal("160"); 
+    expect ((await darc.getTokenOwnerBalance(0, target2)).toBigInt().toString()).to.equal("90");
+    expect ((await darc.getTokenOwnerBalance(1, target3)).toBigInt().toString()).to.equal("160"); 
   });
 });
