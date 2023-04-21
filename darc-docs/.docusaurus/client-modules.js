fb13d04622466d4ea8c987afb89b691a6d9ed3bc
立党 Lidang <wxr001@gmail.com>
2023-04-20 23:29:22 -0500

    move

---
+++ darc-docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/xinranwang/Documents/GitHub/DARC/darc-docs/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/xinranwang/Documents/GitHub/DARC/darc-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.0_@types+node@18.15.13_react-dom@17.0.2_react@17.0.2_ts-node@10.9.1_typescript@5.0.4/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/xinranwang/Documents/GitHub/DARC/darc-docs/node_modules/.pnpm/@docusaurus+theme-classic@2.4.0_@types+node@18.15.13_react-dom@17.0.2_react@17.0.2_ts-node@10.9.1_typescript@5.0.4/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/xinranwang/Documents/GitHub/DARC/darc-docs/src/css/custom.css'),
+];
