6a684aff40a60ac36c17d0b6036d35155238fe88
立党 Lidang <wxr001@gmail.com>
2023-06-01 03:29:47 -0500

    add

---
+++ darc-protocol/contracts/test-contracts/test-voting/VotingTest.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.9;
+import "../../Runtime/VotingMachine/VotingMachine.sol";
+
+
+/**
+ * @title The unit test contract of the voting machine
+ * @author DARC Team
+ * @notice null
+ */
+
+contract VotingTestContract is VotingMachine{
+  
+} 
