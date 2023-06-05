e9e4a72c79ae53bd287bd4741cad357eefbae981
立党 Lidang <wxr001@gmail.com>
2023-06-05 01:43:45 -0500

    Create BatchCreateTokenClassExpressionFunction.sol

---
+++ darc-protocol/contracts/protocol/Plugin/Conditions/Operations/BatchCreateTokenClass/BatchCreateTokenClassExpressionFunction.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.9;
+/**
+ * @title ConditionExpressionFunctions
+ * @author DARC Team
+ * @notice Functions of criteria expression related to Machine State
+ */
+
+
+import "../../../../MachineState.sol";
+import "../../../../MachineStateManager.sol";
+import "../../../../Utilities/StringUtils.sol";
+import "../../../../Utilities/OpcodeMap.sol";
+
+contract BatchCreateTokenClassExpressionFunction is MachineStateManager {
+  
+}
