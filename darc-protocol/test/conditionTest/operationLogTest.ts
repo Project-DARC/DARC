23e97b8bfee9a49b56a755320cda6bade389bd9b
立党 Lidang <6800565+lidangzzz@users.noreply.github.com>
2024-01-18 02:59:21 -0600

    Update operationLogTest.ts

--- darc-protocol/test/conditionTest/operationLogTest.ts
+++ darc-protocol/test/conditionTest/operationLogTest.ts
@@ -13,7 +13,7 @@ const target2 = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
 
 const target3 = "0x870526b7973b56163a6997bb7c886f5e4ea53638";
 
-describe.only("operation log  test", function () {
+describe("operation log  test", function () {
   it("should pass operation log test", async function () {
     const PluginTestFactory = await ethers.getContractFactory(
       "TestBaseContract"
