189aeeb1a232bdce7e9c8f48067d5ca372c78aa0
立党 Lidang <wxr001@gmail.com>
2024-02-05 14:46:16 -0600

    Update expression-test.ts

--- darc-js/tests/expression-test/expression-test.ts
+++ darc-js/tests/expression-test/expression-test.ts
@@ -6,7 +6,7 @@ import 'mocha';
 import { expect } from 'chai';
 import { batch_add_and_enable_plugins, batch_mint_tokens, batch_create_token_classes } from "../../src/SDK/sdk";
 import {or, node, and, expression} from "../../src/SDK/Node";
-
+import { transpileAndRun } from "../../src/SDK/runtime";
 
 const programOperatorAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 
@@ -226,5 +226,146 @@ describe.only('expression node basic test', () => {
       darc_contract_address,
       "test program"
     );
+  });
+
+  it ("should generate condition node successfully in bylaw script runtime", async () => {
+    const darc_contract_address = await deployDARC(DARC_VERSION.Latest, signer);
+
+    const code = `
+
+    const target1 = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
+
+
+    const target2 = '0x90F79bf6EB2c4f870365E785982E1f101E93b906';
+
+
+    batch_create_token_classes(['token_0', 'token_1'],
+    [0,1],
+    [10,20], 
+    [20,30]);
+
+
+    batch_mint_tokens([ "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", 
+    "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"],
+    [0, 1], [100,200]);
+
+    batch_add_and_enable_plugins(
+      [
+        // plugin 0: disable all operations
+        {
+          returnType: BigNumber.from(2), // no
+          level: BigNumber.from(3), // level 3
+          votingRuleIndex: BigNumber.from(0),
+          notes: "disable all program",
+          bIsEnabled: true,
+          bIsInitialized: true,
+          bIsBeforeOperation: true,
+          conditionNodes: new TRUE() // always true
+        },
+        {
+          returnType: BigInt(4), // yes and skip sandbox
+          level: BigInt(103),
+          votingRuleIndex: BigInt(0),
+          notes: "allow operatorAddress == target1 | operatorAddress == target2",
+          bIsEnabled: true,
+          bIsInitialized: true,
+          bIsBeforeOperation: true,
+          conditionNodes:
+          operator_address_equals(target1) | operator_address_equals(target2)
+              // or(
+              //   expression(3, {
+              //     STRING_ARRAY: [],
+              //     UINT256_2DARRAY: [],
+              //     ADDRESS_2DARRAY: [[target1]],
+              //     BYTES: []
+              //   }),
+              //   expression(3, {
+              //     STRING_ARRAY: [],
+              //     UINT256_2DARRAY: [],
+              //     ADDRESS_2DARRAY: [[target2]],
+              //     BYTES: []
+              //   })
+              // )
+        }
+      ]
+    )
+    `;
+    await transpileAndRun(code, signer, darc_contract_address, DARC_VERSION.Latest);
+    // it should 
+
+
+    const attached_local_darc_signer1 = new DARC.DARC({
+      address: darc_contract_address,
+      wallet: signer1,
+      version: DARC_VERSION.Latest,
+    });
+
+    const attached_local_darc_signer2 = new DARC.DARC({
+      address: darc_contract_address,
+      wallet: signer2,
+      version: DARC_VERSION.Latest,
+    });
+
+    // signer 1 and signer 2 should be able to run the program
+    let isSuccess = true;
+    try{
+      await executeOperationList([
+        batch_mint_tokens(
+          [ "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"],
+          toBigIntArray([0, 1]), 
+          toBigIntArray([100,200])
+        )
+      ],
+      signer1,
+      darc_contract_address,
+      "test program"
+      );
+    }
+    catch(e){
+      isSuccess = false;
+    }
+
+    expect (isSuccess).to.equal(true);
+
+    isSuccess = true;
+    try{
+      await executeOperationList([
+        batch_mint_tokens(
+          [ "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"],
+          toBigIntArray([0, 1]), 
+          toBigIntArray([100,200])
+        )
+      ],
+      signer2,
+      darc_contract_address,
+      "test program"
+      );
+    }
+    catch(e){
+      isSuccess = false;
+    }
+
+    expect (isSuccess).to.equal(true);
+
+    // and signer0 and programOperatorAddress should not be able to run the program
+    isSuccess = true;
+    try{
+      await executeOperationList([
+        batch_mint_tokens(
+          [ "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"],
+          toBigIntArray([0, 1]), 
+          toBigIntArray([100,200])
+        )
+      ],
+      signer,
+      darc_contract_address,
+      "test program"
+      );
+    }
+    catch(e){
+
+      isSuccess = false;
+    }
+    expect (isSuccess).to.equal(false);
   });
 });
