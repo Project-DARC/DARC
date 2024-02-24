3e44ec6cb9c3bcd3b8c91c5a7019fca320376e6c
立党 Lidang <wxr001@gmail.com>
2024-02-23 23:51:17 -0600

    Update DARC.ts

--- darc-protocol/test/DARC.ts
+++ darc-protocol/test/DARC.ts
@@ -1,5 +1,4 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import { ethers } from "hardhat";
 
